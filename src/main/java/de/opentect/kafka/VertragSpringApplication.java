cea2d55be03a6dc375a4163f49b8df0617079683
javaansehz <javaansehz@gmail.com>
2018-04-17 20:45:55 +0200

    fix compile error

--- src/main/java/de/opentect/kafka/VertragSpringApplication.java
+++ src/main/java/de/opentect/kafka/VertragSpringApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VertragSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PartnerSpringApplication.class, args);
+        SpringApplication.run(VertragSpringApplication.class, args);
     }
 }
